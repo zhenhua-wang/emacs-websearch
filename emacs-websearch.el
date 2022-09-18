888314692383d257a38ca5242976068acb142ed9
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2022-09-18 04:34:36 -0500

    add docs

--- emacs-websearch.el
+++ emacs-websearch.el
@@ -1,3 +1,7 @@
+;;; emacs-websearch.el --- emacs websearch  -*- lexical-binding: t; -*-
+
+
+
 (require 'cl-lib)
 (require 'dom)
 (require 'json)
@@ -24,26 +28,6 @@
   (pcase 'emacs-websearch-engine
     (google (mapcar #'identity (aref data 1)))))
 
-;; (defun emacs-websearch (input)
-;;   (interactive
-;;    (let ((input-default (thing-at-point 'symbol)))
-;;      (list (read-string (format-prompt (format "Search on %s" emacs-websearch-engine)
-;;                                        input-default)
-;;                         nil nil input-default))))
-;;   (if (not (string-empty-p input))
-;;       (request
-;;         emacs-websearch-suggest
-;;         :type "GET"
-;;         :params (list
-;;                  (cons "client" "firefox")
-;;                  (cons "q" input))
-;;         :parser 'json-read
-;;         :success (cl-function
-;;                   (lambda (&key data &allow-other-keys)
-;;                     (let ((result (completing-read (format "Search on %s " emacs-websearch-engine)
-;;                                                    (emacs-websearch-parse-suggests data))))
-;;                       (browse-url (format emacs-websearch-link result))))))))
-
 (defun emacs-websearch-builder (input &rest pred type)
   (if (not (string-empty-p input))
       (let ((result))
@@ -65,3 +49,7 @@
    (list (completing-read (format "Search on %s: " emacs-websearch-engine)
                           (completion-table-dynamic #'emacs-websearch-builder))))
   (browse-url (format emacs-websearch-link result)))
+
+(provide 'emacs-websearch)
+
+;;; emacs-websearch.el ends here
